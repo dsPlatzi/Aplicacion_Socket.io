d27686fe6b10cd0225d7286cf550b8926758f36d
Boris <munguiahuaman@gmail.com>
2022-12-31 00:05:03 -0500

    Emitir eventos personalizados

--- src/index.js
+++ src/index.js
@@ -18,18 +18,31 @@ app.get('/', (req, res) => {
 
 //evento cuando un cliente se conecta
 io.on('connection', (socket) => {
-    console.log("Clientes conectados", io.engine.clientsCount);
-    console.log("Id del socket conectado", socket.id);
 
-    //evento para detectar cuando un cliente se desconecta
-    socket.on('disconnect', () => {
-        console.log("Id del socket desconectado", socket.id);
+    //eventos disponibles en la libreria
+    // console.log("Clientes conectados", io.engine.clientsCount);
+    // console.log("Id del socket conectado", socket.id);
+
+    // //evento para detectar cuando un cliente se desconecta
+    // socket.on('disconnect', () => {
+    //     console.log("Id del socket desconectado", socket.id);
+    // });
+
+    // //evento para detectar cuando se pasa de http a ws
+    // socket.conn.once("upgrade", () => {
+    //     console.log("Http a ", socket.conn.transport.name);
+    // });
+
+    //emitir un evento al cliente
+    socket.emit("welcome", "Ahora estas conectado");
+
+    //dectectar eventos
+    socket.on("emit-to-server", (data) => {
+        console.log(data);
     });
 
-    //evento para detectar cuando se pasa de http a ws
-    socket.conn.once("upgrade", () => {
-        console.log("Http a ", socket.conn.transport.name);
-    });
+    //emitir un evento a todos los clientes
+    io.emit("everyone", socket.id + " se ha conectado");
 });
 
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,32 +1,52 @@
 const socket = io();
 
 
-const checkSocket = () => {
-    console.log("Estado del socket", socket.connected);
-};
+//eventos disponibles en la libreria  desde el lado del cliente
+// const checkSocket = () => {
+//     console.log("Estado del socket", socket.connected);
+// };
 
 
-//eventos disponibles en la libreria
-socket.on('connect', () => {
-    console.log("Socket conectado", socket.id);
-    checkSocket();
+// //eventos disponibles en la libreria
+// socket.on('connect', () => {
+//     console.log("Socket conectado", socket.id);
+//     checkSocket();
+// });
+
+// socket.on("connect_error", (error) => {
+//     console.log("Error al conectar", error);
+// });
+
+// socket.on('disconnect', () => {
+//     console.log("Socket desconectado", socket.id);
+// });
+
+
+// //evento para detectar cuando un cliente se desconecta
+// //eventos disponibles en el engine de socket.io
+// socket.io.on("reconnect_attempt", () => {
+//     console.log("Intentando reconectar");
+// });
+
+// socket.io.on("reconnect", () => {
+//     console.log("Reconectado");
+// });
+
+
+//dectectar eventos
+socket.on("welcome", (data) => {
+    const etiqueta = document.getElementById("text");
+    etiqueta.innerHTML = data;
+    console.log(data);
 });
 
-socket.on("connect_error", (error) => {
-    console.log("Error al conectar", error);
-});
-
-socket.on('disconnect', () => {
-    console.log("Socket desconectado", socket.id);
+socket.on("everyone", (data) => {
+    console.log(data);
 });
 
 
-//evento para detectar cuando un cliente se desconecta
-//eventos disponibles en el engine de socket.io
-socket.io.on("reconnect_attempt", () => {
-    console.log("Intentando reconectar");
-});
-
-socket.io.on("reconnect", () => {
-    console.log("Reconectado");
+//emitir eventos
+const emit = document.getElementById("emit-to-server");
+emit.addEventListener("click", () => {
+    socket.emit("emit-to-server", "Hola desde el cliente");
 });
